4ca7dabb2f86a7603cb7ac16c1c2120d0bebceb1
Kate <ks13581@gmail.com>
2018-09-23 21:19:23 +0300

    Задание №14: Вынести описание тестового стенда в конфигурационный файл

--- addressbook-web-tests/src/test/java/addresbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/addresbook/tests/TestBase.java
@@ -7,7 +7,9 @@ import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    //protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    //options to run in VMoptions: -ea -Dbrowser=firefox
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
     public void setUp() throws Exception {
