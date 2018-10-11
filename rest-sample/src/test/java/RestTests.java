2236b2a062691f6a5b53514c915af3bc511f3d8c
Kate <ks13581@gmail.com>
2018-10-11 10:46:00 +0300

    Task 20. Bugify check if test has been resolved

--- rest-sample/src/test/java/RestTests.java
+++ rest-sample/src/test/java/RestTests.java
@@ -2,20 +2,23 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
+import com.jayway.restassured.RestAssured;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.message.BasicNameValuePair;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 import java.util.List;
 import java.util.Set;
 
 import static org.testng.AssertJUnit.assertEquals;
 
-public class RestTests {
+public class RestTests extends TestBase {
 
-    @Test
+
     public void testCreateIssue() throws IOException {
         Set<Issue> oldIssues = getIssues();
         Issue newIssue = new Issue().setSubject("Test subj").setDescription("Test description");
@@ -46,4 +49,19 @@ public class RestTests {
         JsonElement parsed = new JsonParser().parse(json);
         return parsed.getAsJsonObject().get("issue_id").getAsInt();
     }
+
+    @Test
+    private void isIssueOpen() throws RemoteException, MalformedURLException {
+        String json = RestAssured.get("http://bugify.stqa.ru/api/issues.json").asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        JsonElement issues = parsed.getAsJsonObject().get("issues");
+        System.out.println(issues);
+        System.out.println(issues.getAsJsonArray().get(0));
+        if (issues.getAsJsonArray().get(0).getAsJsonObject().get("state_name").toString().contains("Open")) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+
 }
--- rest-sample/src/test/java/TestBase.java
+++ rest-sample/src/test/java/TestBase.java
@@ -1,24 +1,32 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.jayway.restassured.RestAssured;
 import org.testng.SkipException;
-
-import java.net.MalformedURLException;
-import java.rmi.RemoteException;
+import org.testng.annotations.BeforeClass;
 
 public class TestBase {
 
-    /*private boolean isIssueOpen(int issueId) throws RemoteException, MalformedURLException {
-        System.out.println(app.soap().getIssuesCategories(issueId));
+    @BeforeClass
+    public void auth(){
+        RestAssured.authentication = RestAssured.basic("288f44776e7bec4bf44fdfeb1e646490", "");
+    }
 
-        if (app.soap().getIssuesCategories(issueId).equals("resolved")){
+    private boolean isIssueOpen(int issueId) {
+        String json = RestAssured.get("http://bugify.stqa.ru/api/issues.json").asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        JsonElement issues = parsed.getAsJsonObject().get("issues");
+        System.out.println(issues);
+        if (issues.getAsJsonArray().get(0).getAsJsonObject().get("state_name").toString().contains("Resolved")) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public void skipIfNotFixed(int issueId) throws RemoteException, MalformedURLException {
+    public void skipIfNotFixed(int issueId) {
         if (isIssueOpen(issueId)) {
             throw new SkipException("Ignored because of issue " + issueId);
         }
-    }*/
+    }
 
 }
